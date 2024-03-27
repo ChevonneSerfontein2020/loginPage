3515b1c70c2a8cc387c2e901f6d4f8a8a9892ab8
chevonnes@runninghill.co.za <chevonnes@runninghill.co.za>
2024-03-27 22:17:58 +0200

    added api calls

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express  = require('express');
 const app = express()
+const bcrypt = require( 'bcrypt' );
 
+app.use(express.json())
+
+const users = []
+
+//array of posts to be displayed
 const posts = [
     {
         username: 'Test Username 1',
@@ -16,8 +22,28 @@ const posts = [
     }
 ]
 
+app.get('/users', (req, res) => {
+    //send back the array of users as a response
+    res.json(users)
+})
+
+app.post('/users', (req, res) => {
+    const user = { 
+        name: req.body.name, 
+        password: req.body.password
+    }
+    users.push(user)
+    res.status(201).send()
+})
+
+// get-call to display posts
 app.get('/posts', (req,res) => {
     res.json(posts)
+})
+
+app.get('/login', (req,res) => {
+    // authenticate the user
+
 })
 
 app.listen(3000)
